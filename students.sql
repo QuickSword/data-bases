d77ec701dbe5fc0a41d8351b8c9e5cd8702bc588
QuickSword <47432363+QuickSword@users.noreply.github.com>
2021-05-24 03:34:03 +0200

    Add files via upload

--- students.sql
+++ students.sql
@@ -1,41 +1,42 @@
-CREATE TABLE wybory(
-	id NUMERIC(9) PRIMARY KEY,
-	nazwa VARCHAR(12) NOT NULL,
-	liczba_posad NUMERIC NOT NULL,
-	termin_zgloszen TIMESTAMP NOT NULL,
-	termin_rozpoczecia TIMESTAMP NOT NULL,
-	termin_zakonczenia TIMESTAMP NOT NULL,
-	CHECK ( liczba_posad > 0 ),
-	CHECK ( termin_rozpoczecia >  wybory.termin_zgloszen),
-	CHECK ( termin_zakonczenia > wybory.termin_rozpoczecia )
+CREATE TABLE wybory
+(
+    id                 NUMERIC(9) PRIMARY KEY,
+    nazwa              VARCHAR(12) NOT NULL,
+    liczba_posad       NUMERIC     NOT NULL,
+    termin_zgloszen    TIMESTAMP   NOT NULL,
+    termin_rozpoczecia TIMESTAMP   NOT NULL,
+    termin_zakonczenia TIMESTAMP   NOT NULL,
+    CHECK ( liczba_posad > 0 ),
+    CHECK ( termin_rozpoczecia > wybory.termin_zgloszen),
+    CHECK ( termin_zakonczenia > wybory.termin_rozpoczecia )
 
 );
 
-CREATE TABLE wyborcy(
-	numer_indeksu NUMERIC(6) PRIMARY KEY,
-	imie VARCHAR(12) NOT NULL,
-	nazwisko VARCHAR(19) NOT NULL,
-	czy_komisja BOOLEAN NOT NULL DEFAULT FALSE
+CREATE TABLE wyborcy
+(
+    numer_indeksu NUMERIC(6) PRIMARY KEY,
+    imie          VARCHAR(12) NOT NULL,
+    nazwisko      VARCHAR(19) NOT NULL,
+    czy_komisja   BOOLEAN     NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE kandydaci(
-	id_wyborow NUMERIC(9) NOT NULL REFERENCES wybory(id),
-	nr_indeksu_wyborcy NUMERIC(6) NOT NULL REFERENCES wyborcy,
-	kandydat NUMERIC(6) NOT NULL PRIMARY KEY REFERENCES wyborcy,
-	czy_wybrany BOOLEAN NOT NULL DEFAULT FALSE
+CREATE TABLE kandydaci
+(
+    id_wyborow         NUMERIC(9) NOT NULL REFERENCES wybory (id),
+    nr_indeksu_wyborcy NUMERIC(6) NOT NULL REFERENCES wyborcy,
+    kandydat           NUMERIC(6) NOT NULL PRIMARY KEY REFERENCES wyborcy,
+    czy_wybrany        BOOLEAN    NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE glosy(
-	id_wyborow NUMERIC(9) NOT NULL REFERENCES wybory(id),
-	nr_indeksu_wyborcy NUMERIC(6) PRIMARY KEY REFERENCES wyborcy(numer_indeksu),
-	kandydat NUMERIC(6) REFERENCES kandydaci(kandydat)
+CREATE TABLE glosy
+(
+    id                 SERIAL     NOT NULL PRIMARY KEY,
+    id_wyborow         NUMERIC(9) NOT NULL REFERENCES wybory (id),
+    nr_indeksu_wyborcy NUMERIC(6) REFERENCES NOT NULL wyborcy (numer_indeksu),
+    kandydat           NUMERIC(6) REFERENCES kandydaci (kandydat)
 );
 
-CREATE TABLE wyniki(
-  id_wyborow NUMERIC(9) NOT NULL REFERENCES wybory(id),
-  kandydat NUMERIC(6) NOT NULL PRIMARY KEY REFERENCES  kandydaci(kandydat),
-  liczba_glosow NUMERIC
-);
+
 
 
 
